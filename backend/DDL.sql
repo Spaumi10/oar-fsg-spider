3c0c6bea65a1e451f4fb404d1ae13c98ae0f8c7c
Michael Spaulding <mikespaulding3@gmail.com>
2024-07-28 20:00:24 -0700

    Started DB work

---
+++ backend/DDL.sql
@@ -0,0 +1,24 @@
+-- --------------
+-- Table crimes
+-- Stores each crime and accompanying information
+-- --------------
+CREATE TABLE IF NOT EXISTS crimes(
+    crime_id INTEGER PRIMARY KEY,
+    crime_name TEXT NOT NULL,
+    ors TEXT NOT NULL UNIQUE,
+    ranking INTEGER,
+    crime_type_id INTEGER,
+    FOREIGN KEY (crime_type_id)
+        REFERENCES crime_types
+        ON DELETE CASCADE
+);
+
+
+-- --------------
+-- Table crime_types
+-- Stores crime types 
+-- --------------
+CREATE TABLE IF NOT EXISTS crime_types(
+    crime_type_id INTEGER PRIMARY KEY,
+    crime_type TEXT NOT NULL
+);
